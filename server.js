fdec66b25ab2d91c38821fac598c2871a079761d
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2019-10-31 08:30:18 +0100

    Connect the app to database

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
+//Connection to DB
+connectDB();
+
 //Define routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
-
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 app.get("/", (req, res) =>
   res.json({ message: "Welcome to the contact-keeper API..." })
@@ -16,4 +18,4 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`The Server is running on port:: ${PORT} `);
-}); 
+});
